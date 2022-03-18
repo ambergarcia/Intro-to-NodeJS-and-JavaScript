e4010919fb2ad508f52149efcbfe43b1749873ce
Amber Garcia <77137171+ambergarcia@users.noreply.github.com>
2022-03-18 11:31:12 -0400

    Workshop Attendee Files
    Files specifically for the attendee to modify while we run through the workshop.

---
+++ Tarot.js
@@ -0,0 +1,66 @@
+const tarot_filename = `./tarot-deck.json`;
+const fs = require('fs')
+const Tarot = class Tarot{
+    /*━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    Code-Along 8: Writing Classes
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/
+ 
+
+
+
+
+
+
+    /*━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    Exercise 8: Obtain a card
+     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/
+
+
+
+
+
+
+
+    /*━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    Exercise 9: Find a card given an index
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/
+
+
+
+
+
+
+
+    /*━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    Exercise 10: Get all cards drawn on date
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/
+
+
+
+}
+
+// Test your code using the class itself.
+
+fs.access(tarot_filename,(err)=>{
+    if(err){
+        console.error(`${err} Tarot doesn't exist, unable to instantiate variables.`);
+    }else{
+        fs.readFile(tarot_filename,(err, data) => {
+            if(err){
+                console.error(err);
+            }else{
+                let tarot_deck = JSON.parse(data);
+                tarot_deck = tarot_deck[`Tarot-Deck`];
+
+                let tarot = new Tarot(tarot_deck);
+                
+                //tarot.draw_card(3);
+                //tarot.find_card(23);
+                //tarot.clear();
+            }
+        });
+    }
+});
+
+
+module.exports = Tarot;
